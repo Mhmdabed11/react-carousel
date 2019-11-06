8204eed70c10200547829bd5a9f7382e2e659738
MhmdAbed11 <mohammad_aabed@hotmail.com>
2019-11-07 00:45:10 +0200

    add more edge cases

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./carousel.scss";
 
-const NUM: number = 6;
+const NUM: number = 5;
 const TIME: number = 600;
 
 type Movie = {
   color: string | number;
-  key: number | string;
+  key: number;
   movie: string;
   title: number;
 };
 
-export default function Carousel() {
+export default function Carousel({ slides }: { slides: any }) {
   const sliderRef = React.useRef(null);
   const [isSliding, setSliding] = React.useState<boolean>(false);
   const [direction, setDirection] = React.useState<number>(0);
@@ -22,117 +22,30 @@ export default function Carousel() {
   );
   const [leftEdgeIndex, setLeftEdgeIndex] = React.useState<number>(0);
   const [slidesToMove, setSlidesToMove] = React.useState<number>(activeSlides);
-  const [movies, setMovies] = React.useState<Movie[]>([
-    {
-      title: 0,
-      key: 0,
-      movie:
-        "https://occ-0-3409-2773.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABaxvzq-QrYi7gL4wOyOBc63ao-5441L6LgSYJ5KfsOAedVyIDHPgkNXyU8xuKD6lRdr7CrN2Eg9WtfBW6yRCHDpdBWjuFbEQ.webp?r=88d",
-      color: "red"
-    },
-    {
-      title: 1,
-      key: 1,
-      movie:
-        "https://occ-0-3409-2773.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABd0Ng-XYm6lVJoQkjZv1fRFK-eEpRGrYZwqv6g4R4mMbaRntHFu5D_iVxuZEiFs74hKt4RTzYqRv8vgsrBqcqZEpSrGDTBV-.webp?r=e8f",
-      color: "green"
-    },
-    {
-      title: 2,
-      key: 2,
-      movie:
-        "https://occ-0-3409-2773.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABQTuEMRqug-Pb0-w5xE3z0gFDSOpKVcVWNu-qwqwa0TfN0zffHhr2b2NBZdnK8t3EEI8M7ESi4dBQ1SWBVADjNFzAQ_8TiyH.webp?r=72f",
-      color: "yellow"
-    },
-    {
-      title: 3,
-      key: 3,
-      movie:
-        "https://occ-0-3409-2773.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABYHPj_fHm-MQx-fxFppjfWz8UJY0WwurrKOoV2Q4GJ2oKYWgaqZoVb-HxDyXdX0eJU81Xv2CS_KeLsdpG2wENCvbPEQRPT50MpeCJjKbJIYEtTlKgVY3L8SzCy5BbYYwXg.jpg?r=6b9",
-      color: "blue"
-    },
-    {
-      title: 4,
-      key: 4,
-      movie:
-        "https://occ-0-3409-2773.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABTHSLjT6Oqkl4TS2OSEuBhIfW6xB5-pyEOlIzoeNtuL_OLB_32ogRbGlNMKQ-y6kbzm6_QiMl7_h3kqQgBlo6UWkrWJ33G0s.jpg?r=854",
-      color: "orange"
-    },
-    {
-      title: 5,
-      key: 5,
-      movie:
-        "https://occ-0-3409-2773.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABQBZ-Hs2sbqvNzoMK_NNzhLV29bfUGZ3zfa0VMRZRoWC-tzDy_Zr3Pa9EEkcdEnvOy0XDj_UvepaT5P22XnNscJ_gyLKziXB.webp?r=4fb",
-      color: "purple"
-    },
-    {
-      title: 6,
-      key: 6,
-      movie:
-        "https://occ-0-3409-2773.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABSO0Uahpb8-OKaMrCJ0ByzZjl0smJiIGc3nPCNtQWaV3H29fjwy0IfQ-tAzdpz73Yp6nJ0ivR4t2ngSF8SWtXHz_kDTBKoPA.webp?r=4de",
-      color: "red"
-    }
-    // {
-    //   title: 7,
-    //   key: 7,
-    //   movie:
-    //     "https://occ-0-3409-2773.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABXdkA5mmI9OxER465PCbCmRFVdyxGl3TclzKgZm_6Pn7hyVsy1FvAPtsbPMsoKZ5H56EPsnFZlqmEdXf_rmz98fEvQ6WZhxE.jpg?r=e7b",
-    //   color: "black"
-    // },
-    // {
-    //   title: 8,
-    //   key: 8,
-    //   movie:
-    //     "https://occ-0-3409-2773.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABdUHnWCCn29uk30cE_YipZxY2DU-KsJGJ1PvWpULKDHL0MMdpnlzo-iFlJc-FRGfrwyxLcvNC7YOZGYTtwK8zNRpJ-7_2Wx7.jpg?r=de9",
-    //   color: "white"
-    // },
-    // {
-    //   title: 9,
-    //   key: 9,
-    //   movie:
-    //     "https://occ-0-3409-300.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABf23LNHN5pkUwKm4Y-HVwnHmqlWBZY07m909lmJ81gd6ZRYoD4klGGXpl6oobb0UXQDt2PGKsyDou17mhin13jEPdkXoAIFWj4rNV_JhgksC6QHFr5TNZRwsckIU.jpg?r=978",
-    //   color: "cyan"
-    // },
-    // {
-    //   title: 10,
-    //   key: 10,
-    //   movie:
-    //     "https://occ-0-3409-300.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABewXZBFgqhncFj1bH7-RYmdeq_nDlM_QxK01ZC6qMg8y45vtzrR2KC0jU_VSUZElYnqN6UMHacJZxAbvvujrwGlWQD6bjGFZEITX6bcopOZHMwVKaKq2aop_6Q3-.jpg?r=7f0",
-    //   color: "#176dff"
-    // },
-    // {
-    //   title: 11,
-    //   key: 11,
-    //   movie:
-    //     "https://occ-0-3409-300.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABe7nQ-4uFaozqJT77LMhmej6PcaIPm3K_HNgPZD0b0n9dVz4E61H8cxiRxX6aGEZN7Srqimm1LTX1N7V-jMfCdEs-r3QaIlwqjbhsLY0XVu9V2MtzBDY7adyvXyk.jpg?r=680",
-    //   color: "#135cdd"
-    // },
-    // {
-    //   title: 12,
-    //   key: 12,
-    //   movie:
-    //     "https://occ-0-3409-300.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABTq_xlD7mWNAxPZhErXQ-yvHOtSLy_yZkzM4aGPfadUkBoouXmuxLz2VJaNEQzr9OALFighp_ZZNM72B-KuESfLbotDX1j_ejXcpGhfVQnZ0eWDUEM6cydtOJh70NmBy9Q.webp?r=3db",
-    //   color: "#efefef"
-    // },
-    // {
-    //   title: 13,
-    //   key: 13,
-    //   movie:
-    //     "https://occ-0-3409-300.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABWFE5iihqfc9JBHE0X-KSo581HCrf63U0rTcLj6IkY-HhG6qz7-rBVu1SiGNek1Y0CmbtSxOCu5wwymxgKr4g7IHuTS6pm1MT_zkXz88Os5gzyNzFX75fgpycpy-.jpg?r=0dc",
-    //   color: "#e5fded"
-    // }
-  ]);
+  const [movies, setMovies] = React.useState<Movie[]>(slides);
 
-  let length = 0;
-  if (hasMovedOnce) {
-    length = movies.length / 2;
-  } else {
-    length = movies.length;
-  }
+  let length = slides.length;
 
   React.useEffect(() => {
     console.log("MOVIES ", movies);
   }, [movies]);
+
+  React.useEffect(() => {
+    if (slides.length <= 2 * activeSlides && length > activeSlides) {
+      let newMovies = [...movies];
+      //@ts-ignore
+      newMovies = newMovies.map(movie => ({
+        ...movie,
+        key: movie.key + 1000
+      }));
+      let newMoviesCopy = [...newMovies];
+      newMoviesCopy = newMoviesCopy.map(movie => ({
+        ...movie,
+        key: movie.key + 2000
+      }));
+      setMovies([...movies, ...newMovies, ...newMoviesCopy]);
+    }
+  }, [slides, activeSlides]);
 
   // add event listener to window
   React.useEffect(() => {
@@ -154,6 +67,7 @@ export default function Carousel() {
   }, [width, activeSlides]);
 
   const x = 100 + 100 / activeSlides;
+
   const styles =
     hasMovedOnce && !isSliding
       ? { transform: `translateX(-${x}%)` }
@@ -162,12 +76,25 @@ export default function Carousel() {
           transform: `translateX(-${x + slidesToMove * (100 / activeSlides)}%)`,
           transition: `all ${TIME}ms ease`
         }
-      : !hasMovedOnce && isSliding && direction === 1
+      : !hasMovedOnce &&
+        isSliding &&
+        direction === 1 &&
+        length <= 2 * activeSlides &&
+        slidesToMove < activeSlides
+      ? {
+          transform: `translateX(-${(slidesToMove * 100) / activeSlides}%)`,
+          transition: `all ${TIME}ms ease`
+        }
+      : !hasMovedOnce &&
+        isSliding &&
+        direction === 1 &&
+        length > 2 * activeSlides
       ? {
           transform: `translateX(-${100}%)`,
           transition: `all ${TIME}ms ease`
         }
-      : isSliding && direction === -1 && slidesToMove !== 0
+      : //BACKWARDS
+      isSliding && direction === -1 && slidesToMove !== 0
       ? {
           transform: `translateX(-${x - slidesToMove * (100 / activeSlides)}%)`,
           transition: `all ${TIME}ms ease`
@@ -205,6 +132,7 @@ export default function Carousel() {
       setSlidesToMove(activeSlides);
       slidesToShift = activeSlides;
     }
+    console.log("SLIDES TO MOVE", slidesToShift);
     // update state after specific time to make slidinjg illusion
     setTimeout(() => {
       if (hasMovedOnce) {
@@ -224,24 +152,39 @@ export default function Carousel() {
         moviesCopy = [...moviesCopy, ...tempArray];
         setMovies(moviesCopy);
       }
-      setSliding(false);
       if (!hasMovedOnce) {
-        let newMovies: Movie[] = [...movies];
-        newMovies = newMovies.map(item => ({
-          ...item,
-          key: item.title + 20
-        }));
-        let newMoviesCopy: Movie[] = [...newMovies];
-        if (
-          newMoviesCopy &&
-          Array.isArray(newMoviesCopy) &&
-          newMoviesCopy.length > 0
-        ) {
-          let movieAtEndOfArray: Movie = newMoviesCopy.pop()!;
-          setMovies(movies => [movieAtEndOfArray, ...movies, ...newMoviesCopy]);
+        if (length > 2 * activeSlides) {
+          let newMovies: Movie[] = [...movies];
+          newMovies = newMovies.map(item => ({
+            ...item,
+            key: item.title + 20
+          }));
+          let newMoviesCopy: Movie[] = [...newMovies];
+          if (
+            newMoviesCopy &&
+            Array.isArray(newMoviesCopy) &&
+            newMoviesCopy.length > 0
+          ) {
+            let movieAtEndOfArray: Movie = newMoviesCopy.pop()!;
+            setMovies(movies => [
+              movieAtEndOfArray,
+              ...movies,
+              ...newMoviesCopy
+            ]);
+          }
+        } else {
+          let moviesCopy = [...movies];
+          let tempArray = [];
+          console.log(slidesToShift);
+          for (let i = 0; i < 2 * slidesToShift - 1; i++) {
+            tempArray.push(moviesCopy.shift());
+          }
+          //@ts-ignore
+          setMovies([...moviesCopy, ...tempArray]);
         }
         setHasMovedOnce(true);
       }
+      setSliding(false);
       setDirection(0);
     }, TIME);
   };
