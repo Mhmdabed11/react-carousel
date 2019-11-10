57dc06262bd86d9d1a606282b5af36301da2912c
MhmdAbed11 <mohammad_aabed@hotmail.com>
2019-11-10 16:54:40 +0200

    Updates

---
+++ precache-manifest.aca7385a9605bf8c7f155aa1f778e85f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17400418436947d58abdf0396461a3f0",
+    "url": "/react-carousel/index.html"
+  },
+  {
+    "revision": "9fd6748fda537ba5f6e6",
+    "url": "/react-carousel/static/css/main.6579649d.chunk.css"
+  },
+  {
+    "revision": "6ada35e83f757a77f3d8",
+    "url": "/react-carousel/static/js/2.24d4dfe3.chunk.js"
+  },
+  {
+    "revision": "9fd6748fda537ba5f6e6",
+    "url": "/react-carousel/static/js/main.c47c51c8.chunk.js"
+  },
+  {
+    "revision": "021577e65ca81b0affe9",
+    "url": "/react-carousel/static/js/runtime-main.2cf76c1f.js"
+  }
+]);
